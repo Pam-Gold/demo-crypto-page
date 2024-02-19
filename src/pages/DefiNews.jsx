2685f14e74ae5dd7fbec5d7ec7a3e1c37e8e7f8d
PamGold <badtrailz@gmail.com>
2024-02-19 06:22:25 +0100

    Edited News Sections

--- src/pages/DefiNews.jsx
+++ src/pages/DefiNews.jsx
@@ -3,39 +3,45 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import NewsBox from "../components/news-props/NewsBox";
 const DefiNews = () => {
+  const [defiNewsData, setDefiNewsData] = useState([]);
 
-  const [defiNewsData, setDefiNewsData] = useState([])
+  const fetchDefiNewsData = async () => {
+    try {
+      const res = await axios.get(
+        `https://newsapi.org/v2/everything?q=defi&pageSize=24&apiKey=05a168e910a246c493f80df4a4f7601d`
+      );
 
-  useEffect(()=>{
-    const fetchDefiNewsData = async () => {
-      try {
-        const res = await axios.get(
-          `https://newsapi.org/v2/everything?q=defi&pageSize=24&apiKey=05a168e910a246c493f80df4a4f7601d`
-        );
+      setDefiNewsData(res.data.articles);
+    } catch (err) {
+      console.warn(err);
+    }
+  };
 
-        setDefiNewsData(res.data.articles);
-      } catch (err) {
-        console.warn(err);
-      }
-    };
+  useEffect(() => {
+    fetchDefiNewsData();
+  }, []);
+  return (
+    <Flex h="fit-content" w="100%" flexDir="column" rowGap="20px">
+      <Heading fontSize="1.6rem" textDecor="underline">
+        Defi News
+      </Heading>
 
-    fetchDefiNewsData()
-  }, [])
-  return(
-<Flex h="fit-content" w="100%" flexDir="column" rowGap="20px" >
-<Heading fontSize="1.6rem" textDecor="underline">
-  Defi News
-</Heading>
-
-
-      <Grid  gridTemplateColumns={{base:"1fr", sm:"1fr", md:"1fr 1fr", lg:"1fr 1fr 1fr", xl:"1fr 1fr 1fr 1fr"}} gridTemplateRows="min-content" gap="15px">
-
-
-
-  {defiNewsData.map(news=><NewsBox {...news}/>)}
-
+      <Grid
+        gridTemplateColumns={{
+          base: "1fr",
+          sm: "1fr",
+          md: "1fr 1fr",
+          lg: "1fr 1fr 1fr",
+          xl: "1fr 1fr 1fr 1fr",
+        }}
+        gridTemplateRows="min-content"
+        gap="15px"
+      >
+        {defiNewsData.map((news) => (
+          <NewsBox {...news} />
+        ))}
       </Grid>
-      </Flex>
-  )
-}
+    </Flex>
+  );
+};
 export default DefiNews;
